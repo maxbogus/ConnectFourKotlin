94c896b2df2d91c6f5140537ae5197c630339d9b
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-05 03:38:03 +0300

    Update Main.kt
    implemented draft version of board draw

--- Main.kt
+++ Main.kt
@@ -1,29 +1,53 @@
 package connectfour
 
-val REGEX = Regex("(^\\s*[5-9]\\s*[xX]\\s*[5-9]\\s*$)")
+val CORRECT_BOARD_SIZE_REGEX = Regex("(^\\s*[5-9]\\s*[xX]\\s*[5-9]\\s*$)")
+val INVALID_ROW_BOARD_REGEX = Regex("(^\\s*\\d+\\s*[xX]\\s*[5-9]\\s*\$)")
+val INVALID_COLUMN_BOARD_REGEX = Regex("(^\\s*[5-9]\\s*[xX]\\s*\\d+\\s*\$)")
 
 fun main() {
     println("Connect Four")
+    val (rows, columns) = drawBoard()
+    drawBoard(rows, columns)
+}
+
+private fun drawBoard(rows: Int, columns: Int) {
+    for (row in 0..rows) {
+        when (row) {
+            0 -> {
+                println("start".repeat(columns))
+            }
+            rows -> {
+                println("end".repeat(columns))
+            }
+            else -> {
+                println("middle".repeat(columns))
+            }
+        }
+    }
+}
+
+private fun drawBoard(): Pair<Int, Int> {
     println("First player's name:")
     val firstPlayerName = readLine()!!
     println("Second player's name:")
     val secondPlayerName = readLine()!!
-    var rows = '6'
-    var columns = '7'
+    var rows = 6
+    var columns = 7
     var errorMessage = ""
     do {
         showBoardDemensionsMessage(errorMessage)
         val userInput = readLine()!!
-        if (!userInput.matches(REGEX)) {
+        if (!userInput.matches(CORRECT_BOARD_SIZE_REGEX)) {
             errorMessage = getErrorMessage(userInput)
         } else {
             val list = userInput.filter { it.isDigit() }
-            rows = list.first()
-            columns = list.last()
+            rows = list.first().toInt()
+            columns = list.last().toInt()
         }
-    } while (userInput != "" && !userInput.matches(REGEX))
+    } while (userInput != "" && !userInput.matches(CORRECT_BOARD_SIZE_REGEX))
     println("$firstPlayerName VS $secondPlayerName")
     println("$rows X $columns board")
+    return Pair(rows, columns)
 }
 
 private fun showBoardDemensionsMessage(errorMessage: String) {
@@ -35,9 +59,9 @@ private fun showBoardDemensionsMessage(errorMessage: String) {
 }
 
 private fun getErrorMessage(string: String): String {
-    return if (string.matches(Regex("(^\\s*\\d+\\s*[xX]\\s*[5-9]\\s*\$)"))) {
+    return if (string.matches(INVALID_ROW_BOARD_REGEX)) {
         "Board rows should be from 5 to 9"
-    } else if (string.matches(Regex("(^\\s*[5-9]\\s*[xX]\\s*\\d+\\s*\$)"))) {
+    } else if (string.matches(INVALID_COLUMN_BOARD_REGEX)) {
         "Board columns should be from 5 to 9"
     } else {
         "Invalid input"
